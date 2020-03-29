af4f57550d023b3850aba94d6a70a58bf0ff4430
Bonnie <user_reg@qq.com>
2020-03-29 10:29:09 +0800

    3.29-test-3

--- public_new/js/initialLoad.js
+++ public_new/js/initialLoad.js
@@ -1,4 +1,9 @@
-(function () {
-    var observer = lozad();
-    observer.observe();
-})();
+require(['config'], function () {
+    require(['lozad'], function (lozad) {
+        (function () {
+            console.log(111);
+            var observer = lozad();
+            observer.observe();
+        })();
+    });
+});
